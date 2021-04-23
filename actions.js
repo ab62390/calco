f2a6ba1cfaaddaa5c5306568dafbe29d1767e033
ab62390 <ab0962390222@gmail.com>
2021-04-23 22:10:44 +0200

    init

---
+++ actions.js
@@ -0,0 +1,96 @@
+digit0.addEventListener('click', () => {
+  addToScreen('0')
+});
+digit1.addEventListener('click', () => {
+  addToScreen('1')
+});
+digit2.addEventListener('click', () => {
+  addToScreen('2')
+});
+digit3.addEventListener('click', () => {
+  addToScreen('3')
+});
+digit4.addEventListener('click', () => {
+  addToScreen('4')
+});
+digit5.addEventListener('click', () => {
+  addToScreen('5')
+});
+digit6.addEventListener('click', () => {
+  addToScreen('6')
+});
+digit7.addEventListener('click', () => {
+  addToScreen('7')
+});
+digit8.addEventListener('click', () => {
+  addToScreen('8')
+});
+digit9.addEventListener('click', () => {
+  addToScreen('9')
+});
+
+backspaceButton.addEventListener('click', () => {
+  const str = screen.innerHTML;
+  const newStr = str.substring(0, str.length - 1);
+  screen.innerHTML = newStr;
+});
+
+clearButton.addEventListener('click', ()=>{
+  screen.innerHTML = '';
+});
+
+addButton.addEventListener('click', ()=> {
+  if (isScreenEndWithDigit() && !isScreenEmpty()) addToScreen('+');
+});
+
+subButton.addEventListener('click', () => {
+  if (isScreenEndWithDigit()) addToScreen('-');
+});
+
+divButton.addEventListener('click', () => {
+  if (isScreenEndWithDigit() && !isScreenEmpty()) addToScreen('÷');
+});
+
+multButton.addEventListener('click', () => {
+  if (isScreenEndWithDigit() && !isScreenEmpty()) addToScreen('×');
+});
+
+dotButton.addEventListener('click', () => {
+  if (isScreenEndWithDigit()) addToScreen('.');
+});
+
+equalButton.addEventListener('click', calculate);
+
+/* FUNCTIONS */
+function addToScreen(t) {
+  screen.innerHTML += t;
+  screen.scrollLeft += 50;
+}
+
+function isScreenEmpty() {
+  if (screen.innerHTML == '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isScreenEndWithDigit() {
+  const str = screen.innerHTML;
+  const lastChar = str.substring(str.length - 1);
+  if (['+', '-', '×', '÷', '.'].includes(lastChar)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function calculate() {
+  const str = screen.innerHTML;
+  const str_clean = str
+    .replace(/-$|\+$|×$|÷$/, '')
+    .replace('×', '*')
+    .replace('÷', '/');
+  const res = eval(str_clean);
+  screen.innerHTML = res;
+}
---
+++ selectors.js
@@ -0,0 +1,42 @@
+const digit0 = document
+  .querySelector('#digit-0');
+const digit1 = document
+  .querySelector('#digit-1');
+const digit2 = document
+  .querySelector('#digit-2');
+const digit3 = document
+  .querySelector('#digit-3');
+const digit4 = document
+  .querySelector('#digit-4');
+const digit5 = document
+  .querySelector('#digit-5');
+const digit6 = document
+  .querySelector('#digit-6');
+const digit7 = document
+  .querySelector('#digit-7');
+const digit8 = document
+  .querySelector('#digit-8');
+const digit9 = document
+  .querySelector('#digit-9');
+
+const screen = document
+  .querySelector('#screen');
+
+const clearButton = document
+  .querySelector('#clear-button');
+const backspaceButton = document
+  .querySelector('#backspace-button');
+
+const addButton = document
+  .querySelector('#add-button');
+const subButton = document
+  .querySelector('#sub-button');
+const divButton = document
+  .querySelector('#div-button');
+const multButton = document
+  .querySelector('#mult-button');
+
+const equalButton = document
+  .querySelector('#equal-button');
+const dotButton = document
+  .querySelector('#dot-button');
